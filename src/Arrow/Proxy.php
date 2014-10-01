0c812bfb09bd61506e2d5c99f2690607682424f5
Peter Caulfield <peterjcaulfield@gmail.com>
2014-10-01 21:25:17 +0100

    base methods added

--- src/Arrow/Proxy.php
+++ src/Arrow/Proxy.php
@@ -6,8 +6,35 @@ abstract class Proxy
 {
     protected static $app;
 
-    public static function setProxyApplication($app)
+    protected static $resolvedInstance;
+
+    public function getProxyAccessor();
+
+    public static function setProxiedApplication($app)
     {
         static::$app = $app;
+    }
+
+    protected function getProxiedObject()
+    {
+        return static::resolveProxyInstance(static::getProxyAccessor());
+    }
+
+    protected static function resolveProxyInstance($name)
+    {
+        if (isset(static::$resolvedInstance[$name])) {
+            return static::$resolvedInstance[$name];
+        }
+
+        return static $resolvedInstance[$name] = static::$app[$name];
+    }
+
+    public function __callStatic($method, $args)
+    {
+        $instance = static::getProxiedObject();
+
+        if (!count($args)) return $instance->method();
+
+        return call_user_func_array(array($instance, $method), $args);
     }
 }
